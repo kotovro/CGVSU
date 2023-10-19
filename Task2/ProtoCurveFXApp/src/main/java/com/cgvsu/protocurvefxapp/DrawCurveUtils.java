dbbd2a98f6c6435a77e27910726ff176d991f266
Kotvoro <kotvoro@gmail.com>
2023-10-19 18:44:09 +0300

    Successfully implented Wu rasterisation

--- Task2/ProtoCurveFXApp/src/main/java/com/cgvsu/protocurvefxapp/DrawCurveUtils.java
+++ Task2/ProtoCurveFXApp/src/main/java/com/cgvsu/protocurvefxapp/DrawCurveUtils.java
@@ -21,9 +21,9 @@ public class DrawCurveUtils {
                 curT = endT;
             }
             MutablePoint2D newPoint = solvePolynomial(curT, points);
-            lineBresenham(graphicsContext, (int) curPoint.getX(), (int) curPoint.getY(), (int) newPoint.getX(), (int) newPoint.getY());
+            //lineBresenham(graphicsContext, (int) curPoint.getX(), (int) curPoint.getY(), (int) newPoint.getX(), (int) newPoint.getY());
             //graphicsContext.strokeLine(curPoint.getX(), curPoint.getY(), newPoint.getX(), newPoint.getY());
-            //lineWu(graphicsContext, curPoint.getX(), curPoint.getY(), newPoint.getX(), newPoint.getY());
+            lineWu(graphicsContext, curPoint.getX(), curPoint.getY(), newPoint.getX(), newPoint.getY());
             curPoint = newPoint;
         }
     }
@@ -81,73 +81,64 @@ public class DrawCurveUtils {
         PixelWriter pixelWriter = graphicsContext.getPixelWriter();
         pixelWriter.setColor((int) x, (int) y, new Color(0, 0, 0, c));
     }
-
-    private static int ipart(double x) {
-        return (int) x;
-    }
-
-    private static double fpart(double x) {
-        return x % 1;
-    }
-
-    private static double rfpart(double x) {
-        return 1.0 - fpart(x);
-    }
     private static void lineWu(GraphicsContext graphicsContext, double x0, double y0, double x1, double y1) {
-        boolean steep = abs(y1 - y0) > abs(x1 - x0);
-        if (steep)
-            lineWu(graphicsContext, y0, x0, y1, x1);
-
-        if (x0 > x1)
-            lineWu(graphicsContext, x1, y1, x0, y0);
-
-        double dx = x1 - x0;
-        double dy = y1 - y0;
-        double gradient = dy / dx;
-
-        // handle first endpoint
-        double xend = Math.round(x0);
-        double yend = y0 + gradient * (xend - x0);
-        double xgap = rfpart(x0 + 0.5);
-        double xpxl1 = xend; // this will be used in the main loop
-        double ypxl1 = ipart(yend);
-
-        if (steep) {
-            plot(graphicsContext, ypxl1, xpxl1, rfpart(yend) * xgap);
-            plot(graphicsContext, ypxl1 + 1, xpxl1, fpart(yend) * xgap);
-        } else {
-            plot(graphicsContext, xpxl1, ypxl1, rfpart(yend) * xgap);
-            plot(graphicsContext, xpxl1, ypxl1 + 1, fpart(yend) * xgap);
-        }
-
-        // first y-intersection for the main loop
-        double intery = yend + gradient;
-
-        // handle second endpoint
-        xend = Math.round(x1);
-        yend = y1 + gradient * (xend - x1);
-        xgap = fpart(x1 + 0.5);
-        double xpxl2 = xend; // this will be used in the main loop
-        double ypxl2 = ipart(yend);
-
-        if (steep) {
-            plot(graphicsContext, ypxl2, xpxl2, rfpart(yend) * xgap);
-            plot(graphicsContext, ypxl2 + 1, xpxl2, fpart(yend) * xgap);
-        } else {
-            plot(graphicsContext, xpxl2, ypxl2, rfpart(yend) * xgap);
-            plot(graphicsContext, xpxl2, ypxl2 + 1, fpart(yend) * xgap);
-        }
-
-        // main loop
-        for (double x = xpxl1 + 1; x <= xpxl2 - 1; x++) {
-            if (steep) {
-                plot(graphicsContext, ipart(intery), x, rfpart(intery));
-                plot(graphicsContext, ipart(intery) + 1, x, fpart(intery));
-            } else {
-                plot(graphicsContext, x, ipart(intery), rfpart(intery));
-                plot(graphicsContext, x, ipart(intery) + 1, fpart(intery));
+        double lenX = Math.abs(x1 - x0);
+        double lenY = Math.abs(y1 - y0);
+        double epsilon = 0.00003;
+        if (lenX > lenY) { // линия скорее "горизонтальная"
+            if (x1 < x0) {
+                double temp = x1;
+                x1 = x0;
+                x0 = temp;
+                temp = y1;
+                y1 = y0;
+                y0 = temp;
             }
-            intery = intery + gradient;
+            if (lenY < epsilon) { //линия точно горизонтальная
+                for (double x = x0; x < x1 + 1; x+=1) {
+                    plot(graphicsContext, x, y0, 1);
+                }
+            } else {
+                double deltaY = lenY / lenX;
+                if (y1 < y0) {
+                    deltaY *= -1;
+                }
+                double curY = y0;
+                for (double x = x0; x < x1 + 1; x+=1) {
+                    double yHigh = Math.ceil(curY);
+                    double yLow = Math.floor(curY);
+                    plot(graphicsContext, x, yLow, Math.abs(curY - yHigh));
+                    plot(graphicsContext, x, yHigh, Math.abs(curY - yLow));
+                    curY += deltaY;
+                }
+            }
+        } else { // линия скорее "вертикальная"
+            if (y1 < y0) {
+                double temp = x1;
+                x1 = x0;
+                x0 = temp;
+                temp = y1;
+                y1 = y0;
+                y0 = temp;
+            }
+            if (lenX < epsilon) { //линия точно вертикальная
+                for (double y = y0; y < y1 + 1; y+=1) {
+                    plot(graphicsContext, x0, y, 1);
+                }
+            } else {
+                double deltaX = lenX / lenY;
+                if (x1 < x0) {
+                    deltaX *= -1;
+                }
+                double curX = x0;
+                for (double y = y0; y < y1 + 1; y+=1) {
+                    double xRight = Math.ceil(curX);
+                    double xLeft = Math.floor(curX);
+                    plot(graphicsContext, xLeft, y, Math.abs(curX - xRight));
+                    plot(graphicsContext, xRight, y, Math.abs(curX - xLeft));
+                    curX += deltaX;
+                }
+            }
         }
     }
 
