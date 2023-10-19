e1fc3635aa09b6cbbc1bdea2debfbaef03525da2
Kotvoro <kotvoro@gmail.com>
2023-10-19 03:24:38 +0300

    Unsuccessful Wu rasterisation implementation

--- Task2/ProtoCurveFXApp/src/main/java/com/cgvsu/protocurvefxapp/DrawCurveUtils.java
+++ Task2/ProtoCurveFXApp/src/main/java/com/cgvsu/protocurvefxapp/DrawCurveUtils.java
@@ -23,6 +23,7 @@ public class DrawCurveUtils {
             MutablePoint2D newPoint = solvePolynomial(curT, points);
             lineBresenham(graphicsContext, (int) curPoint.getX(), (int) curPoint.getY(), (int) newPoint.getX(), (int) newPoint.getY());
             //graphicsContext.strokeLine(curPoint.getX(), curPoint.getY(), newPoint.getX(), newPoint.getY());
+            //lineWu(graphicsContext, curPoint.getX(), curPoint.getY(), newPoint.getX(), newPoint.getY());
             curPoint = newPoint;
         }
     }
@@ -64,8 +65,8 @@ public class DrawCurveUtils {
             if (err < 0)
             {
                 err += longShiftCount;
-                x += directionX;//сдвинуть прямую (сместить вверх или вниз, если цикл проходит по иксам)
-                y += directionY;//или сместить влево-вправо, если цикл проходит по y
+                x += directionX;//сдвинуть прямую (сместить вверх или вниз
+                y += directionY;//и сместить влево-вправо)
             }
             else
             {
@@ -76,13 +77,83 @@ public class DrawCurveUtils {
             pixelWriter.setColor(x, y, Color.BLACK);
         }
     }
+    private static void plot(GraphicsContext graphicsContext, double x, double y, double c) {
+        PixelWriter pixelWriter = graphicsContext.getPixelWriter();
+        pixelWriter.setColor((int) x, (int) y, new Color(0, 0, 0, c));
+    }
+
+    private static int ipart(double x) {
+        return (int) x;
+    }
+
+    private static double fpart(double x) {
+        return x % 1;
+    }
+
+    private static double rfpart(double x) {
+        return 1.0 - fpart(x);
+    }
+    private static void lineWu(GraphicsContext graphicsContext, double x0, double y0, double x1, double y1) {
+        boolean steep = abs(y1 - y0) > abs(x1 - x0);
+        if (steep)
+            lineWu(graphicsContext, y0, x0, y1, x1);
+
+        if (x0 > x1)
+            lineWu(graphicsContext, x1, y1, x0, y0);
+
+        double dx = x1 - x0;
+        double dy = y1 - y0;
+        double gradient = dy / dx;
+
+        // handle first endpoint
+        double xend = Math.round(x0);
+        double yend = y0 + gradient * (xend - x0);
+        double xgap = rfpart(x0 + 0.5);
+        double xpxl1 = xend; // this will be used in the main loop
+        double ypxl1 = ipart(yend);
+
+        if (steep) {
+            plot(graphicsContext, ypxl1, xpxl1, rfpart(yend) * xgap);
+            plot(graphicsContext, ypxl1 + 1, xpxl1, fpart(yend) * xgap);
+        } else {
+            plot(graphicsContext, xpxl1, ypxl1, rfpart(yend) * xgap);
+            plot(graphicsContext, xpxl1, ypxl1 + 1, fpart(yend) * xgap);
+        }
+
+        // first y-intersection for the main loop
+        double intery = yend + gradient;
+
+        // handle second endpoint
+        xend = Math.round(x1);
+        yend = y1 + gradient * (xend - x1);
+        xgap = fpart(x1 + 0.5);
+        double xpxl2 = xend; // this will be used in the main loop
+        double ypxl2 = ipart(yend);
+
+        if (steep) {
+            plot(graphicsContext, ypxl2, xpxl2, rfpart(yend) * xgap);
+            plot(graphicsContext, ypxl2 + 1, xpxl2, fpart(yend) * xgap);
+        } else {
+            plot(graphicsContext, xpxl2, ypxl2, rfpart(yend) * xgap);
+            plot(graphicsContext, xpxl2, ypxl2 + 1, fpart(yend) * xgap);
+        }
+
+        // main loop
+        for (double x = xpxl1 + 1; x <= xpxl2 - 1; x++) {
+            if (steep) {
+                plot(graphicsContext, ipart(intery), x, rfpart(intery));
+                plot(graphicsContext, ipart(intery) + 1, x, fpart(intery));
+            } else {
+                plot(graphicsContext, x, ipart(intery), rfpart(intery));
+                plot(graphicsContext, x, ipart(intery) + 1, fpart(intery));
+            }
+            intery = intery + gradient;
+        }
+    }
 
     public static void drawPolynomialCurve(GraphicsContext graphicsContext, ArrayList<ParametrizedPoint> points, Canvas canvas, int pointRadius) {
-        int i = 0;
         graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         for (ParametrizedPoint point : points) {
-            System.out.println("Draw point " + i);
-            i++;
             MutablePoint2D point2D = point.getValue();
             graphicsContext.fillOval(
                     point2D.getX() - pointRadius, point2D.getY() - pointRadius,
